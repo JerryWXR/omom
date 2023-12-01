8a26cc506645cad3d78d98ebbeca39696ac82a8b
wangxinru <A17200310790@163.com>
2023-12-01 14:51:53 +0800

    调整顶部导航

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,20 +4,64 @@ import "./index.css"
 import English from "../../assets/images/English.jpg"
 // @ts-ignore
 import logo from "../../assets/images/logo.jpg"
-import {Button, Modal} from "antd";
+import {Button, Menu, MenuProps, Modal} from "antd";
 import {useNavigate} from "react-router-dom";
 import UnauthenticatedApp from "../../pages/unauthenticated-app";
 
+const items: MenuProps['items'] = [
+    {
+        label: (<div className="headerLogo">
+                <img src={logo} alt=""/>
+            </div>
+        ),
+        key: 'alipay',
+    },
+    {
+        label: '有声课堂',
+        key: '/audible',
 
+    },
+    {
+        label: '背单词',
+        key: '/learnWord',
+    },
+    {
+        label: '雅思',
+        key: '/ielts',
+    },
+    {
+        label: '大学应试英语',
+        key: '/reStudy',
+    },
+    {
+        label: '大学生竞赛',
+        key: '/',
+    },
+    {
+        label: '期末突击',
+        key: '/',
+    },
+    {
+        label: '四六级',
+        key: '/',
+    },
+    {
+        label: '考研',
+        key: '/',
+    },
+    {
+        label: '我的',
+        key: '/mine',
+    },
+
+];
 const Top = () => {
-    const native = useNavigate()
+    const navigate = useNavigate()
     const toLogin = () => {
         // native("/login")
         setIsModalOpen(true);
     }
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-
     const handleCancel = () => {
         setIsModalOpen(false);
     };
@@ -26,48 +70,23 @@ const Top = () => {
             setIsModalOpen(false);
         }
     }
+    const [current, setCurrent] = useState('/audible');
+
+    const onClick: MenuProps['onClick'] = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+        navigate(e.key, {replace: true})
+    };
     return (
         <div className="topBar">
             <div className="header">
                 <div className="wrapper">
-                    <div className="wrapperLeft">
-                        <div className="headerLogo">
-                            <img src={logo} alt=""/>
-                        </div>
-                        <div>
-                            <a href="/audible">有声课堂</a>
-                        </div>
-                        <div>
-                            <a href="/learnWord">背单词</a>
-                        </div>
-                        <div>
-                            <a href="/learnWord">雅思</a>
-                        </div>
-                        <div>
-                            <a href="/reStudy">大学应试英语</a>
-                        </div>
-                        <div>
-                            <a href="/reStudy">大学生竞赛</a>
-                        </div>
-                        <div>
-                            <a href="/reStudy">期末突击</a>
-                        </div>
-                        <div>
-                            <a href="/reStudy">四六级</a>
-                        </div>
-                        <div>
-                            <a href="/reStudy">考研</a>
-                        </div>
-                        <div>
-                            <a href="/mine">我的</a>
-                        </div>
-                    </div>
+                    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items}/>
                     <div className="LoginBtn">
                         <Button onClick={toLogin} size={"small"} type="primary">登录</Button>
                     </div>
                     <Modal footer={null} centered={true} open={isModalOpen} onCancel={handleCancel}>
                         <div style={{width: "400px", textAlign: "center"}}>
-                            {/*<span style={{margin:"0 auto"}}>123</span>*/}
                             <UnauthenticatedApp getIsLogin={getIsLogin}/>
                         </div>
                     </Modal>
